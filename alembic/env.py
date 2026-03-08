08049ac3162bf5deeee8b6279be8b4a3eb1863fb
baltsezhakd <baltsezhakd@gmail.com>
2026-03-08 21:06:20 +0300

    feat: add settings and configure alembic for migrations

--- alembic/env.py
+++ alembic/env.py
@@ -5,12 +5,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.config import settings
+from app.db.base import Base
+from app.models import *
+
 config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = None
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
